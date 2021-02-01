5cd7155513da578da159f56543f35cfb6e4ac957
Jose Riera <rieraja@bc.edu>
2021-02-01 16:22:09 -0500

    2.4 - two buttons

--- two button challenge/ViewController.swift
+++ two button challenge/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func showMessagePressed(_ sender: UIButton) {
+        messageLabel.text = "You are awesome!"
+        messageLabel.textColor = UIColor.blue
+    }
+    @IBAction func showAnotherPressed(_ sender: UIButton) {
+        messageLabel.text = "You are great!"
+        messageLabel.textColor = UIColor.systemRed
 
+    }
+    
 }
 
