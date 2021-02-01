56cf409e1635c01e68ece263912f4910df07cc4a
Jose Riera <rieraja@bc.edu>
2021-02-01 16:09:22 -0500

    Initial Commit

---
+++ two button challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  two button challenge
+//
+//  Created by Jose Riera on 2/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
